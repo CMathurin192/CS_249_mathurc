0782fe63f5f54403fab5769f83093faa88d4b82a
Caleb Mathurin <calebmathurin192@gmail.com>
2024-09-03 18:22:48 -0400

    made code work

--- assign01/src/main/java/edu/mathurc/assign01/Greetings.java
+++ assign01/src/main/java/edu/mathurc/assign01/Greetings.java
@@ -2,6 +2,7 @@ package edu.mathurc.assign01;
 
 public class Greetings {
     public static void main(String [] args) {
-        System.out.println("Hello, Java!");
+        System.out.println("I hate Java.");
+        //Only works with corretto-21 Java compiler or whatever. Not Open JDK 22. Also Python better.
     }
 }
