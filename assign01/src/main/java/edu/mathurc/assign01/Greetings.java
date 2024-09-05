cfd3ee9d353cb7a67a48450d258acfd711153293
Caleb Mathurin <calebmathurin192@gmail.com>
2024-09-05 14:15:36 -0400

    fin

--- assign01/src/main/java/edu/mathurc/assign01/Greetings.java
+++ assign01/src/main/java/edu/mathurc/assign01/Greetings.java
@@ -2,7 +2,9 @@ package edu.mathurc.assign01;
 
 public class Greetings {
     public static void main(String [] args) {
-        System.out.println("I hate Java.");
-        //Only works with corretto-21 Java compiler or whatever. Not Open JDK 22. Also Python better.
+        System.out.println("I miss Python.");
+        //Only works with corretto-21 Java compiler or whatever. Not Open JDK 22.
+        //(Error: LinkageError occurred while loading main class edu.mathurc.assign01.Greetings)
+        //Also Python better.
     }
 }
