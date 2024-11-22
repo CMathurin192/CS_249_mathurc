06e573e084b3283f9f3748a9419042621addfc13
Caleb Mathurin <calebmathurin192@gmail.com>
2024-11-21 22:19:12 -0500

    Through item.java

---
+++ assign06/src/main/java/edu/mathurc/assign06/Drawable.java
@@ -0,0 +1,6 @@
+package edu.mathurc.assign06;
+import edu.mathurc.assign04.GameBoard;
+
+public interface Drawable {
+    public abstract void draw(GameBoard map);
+}
---
+++ assign06/src/main/java/edu/mathurc/assign06/GameFileException.java
@@ -0,0 +1,11 @@
+package edu.mathurc.assign06;
+
+public class GameFileException extends Exception {
+    public GameFileException(String message) {
+        super(message);
+    }
+
+    public GameFileException(String message, Exception e) {
+        super(message, e);
+    }
+}
